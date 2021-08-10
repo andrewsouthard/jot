193af4c625bffdb2a2d880f641e58c559da8e68a
Andrew Southard <andrew.southard@kapokdigital.com>
2021-08-09 20:57:03 -0400

    Setup eslint, prettier, and absolute paths via babel.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import { Header } from 'core/components';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <Header />
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
---
+++ src/core/components/header.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Header } from "react-native-elements";
+
+export default function JotHeader() {
+  return <Header />;
+}
