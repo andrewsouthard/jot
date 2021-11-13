7b07ea7b4d3ba8abde06b56cef755d5c8764eacd
Andrew Southard <andrew.southard@kapokdigital.com>
2021-11-13 14:24:53 -0500

    Add a baisc header.

--- src/core/components/header.tsx
+++ src/core/components/header.tsx
@@ -1,6 +1,39 @@
-import React from "react";
-import { Header } from "react-native-elements";
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Header, Button, Text } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/Feather';
+
+const ICON_SIZE = 30;
 
 export default function JotHeader() {
-  return <Header />;
+  return (
+    <Header containerStyle={styles.header}>
+      <Button
+        buttonStyle={styles.button}
+        icon={<Icon name="menu" size={ICON_SIZE} color="black" />}
+      />
+      <Text h2>JOT</Text>
+      <Button
+        buttonStyle={styles.button}
+        icon={<Icon name="search" size={ICON_SIZE} color="black" />}
+      />
+    </Header>
+  );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    top: 0,
+    left: 0,
+    right: 0,
+    height: '100px',
+    display: 'flex',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    backgroundColor: '#fcfcfc',
+  },
+  button: {
+    backgroundColor: '#fcfcfc',
+    borderColor: 'transparent',
+  },
+});
